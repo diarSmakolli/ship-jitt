227224a7ba5dcd7e696ee56645e7cf295df12978
Dijar Smakolli <dijarprogramming@gmail.com>
2024-05-17 22:43:39 +0200

    feat: fixing bugs in mailgun email sending file, still not set the config for anti spam emails.

--- services/email.js
+++ services/email.js
@@ -12,83 +12,82 @@ const mg = mailgun({
 });
 
 const sendVerificationEmail = async (email, token) => {
-    const mailOptions = {
+    const data = {
         from: 'dijarsmakolli99@gmail.com', // replace with your mailgun verified sender
         to: email,
-        bcc: 'dijarsmakolli50@gmail.com',
         subject: 'Verify your email address',
         html: `<p>Please click <a href="http://localhost:6099/verify-email?token=${token}">here</a> to verify your email address.</p>`
     };
 
-    try {
-        await mg.messages().send(mailOptions);
-        console.log('Verification email sent successfully.');
-    } catch (error) {
-        console.log('error');
-        throw error;
-    }
+    // try {
+    //     await mg.messages().send(mailOptions);
+    //     console.log('Verification email sent successfully.');
+    // } catch (error) {
+    //     console.log('error');
+    //     throw error;
+    // }
+
+    // lets build an promise
+    return new Promise((resolve, reject) => {
+        mg.messages().send(data, (error, body) => {
+            if(error) {
+                reject(error);
+            } else {
+                resolve(body);
+            }
+        })
+    })
+
 };
 
 const sendWelcomeEmail = async(email) => {
-    const mailOptions = {
-        from: 'dijarsmakolli99@gmail.com', // replace with your mailgun verified sender
+    const data = {
+        from: 'dijarsmakolli99@gmail.com',
         to: email,
-        bcc: 'dijarsmakolli50@gmail.com',
-        subject: 'Welcome to our application',
-        html: `Thank you for signing up with us. We are excited to have you on board!`
-    };
-
-    try {
-
-        await mg.messages().send(mailOptions);
-
-    } catch(error) {
-        console.log(error);
-        console.error(error);
-        throw error;
+        subject: 'Welcome to ShipJitt',
+        html: 
+        `Thank you for signing up with us. We are excited to have you on board.`
     }
-};
+
+    return new Promise((resolve,reject) => {
+        mg.messages().send(data, (error, body) => {
+            if(error) {
+                reject(error);
+            } else {
+                resolve(body);
+            }
+        })
+    });
+
+}
 
 const sendPasswordResetEmail = async (email, resetLink) => {
-    const mailOptions = {
+    const data = {
         from: 'dijarsmakolli99@gmail.com', // replace with your mailgun verified sender
         to: email,
-        bcc: 'dijarsmakolli50@gmail.com',
+        bcc: 'dijarsmakolli99@gmail.com',
         subject: 'Reset your password',
         html: `Click the following link to reset your password: <a href="${resetLink}">${resetLink}</a>`
     };
 
-    try {
-        await mg.messages().send(mailOptions);
-        console.log('Password reset email sent successfully.');
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+    // try {
+    //     await mg.messages().send(mailOptions);
+    //     console.log('Password reset email sent successfully.');
+    // } catch (error) {
+    //     console.error(error);
+    //     throw error;
+    // }
+
+    return new Promise((resolve, reject) => {
+        mg.messages().send(data, (error, body) => {
+            if(error) {
+                reject(error);
+            } else {
+                resolve(body);
+            }
+        })
+    })
+
 };
 
 module.exports = { sendVerificationEmail, sendWelcomeEmail, sendPasswordResetEmail };
-
-// send the verification in the email ✅
-// const sendVerificationEmail = async (email, token) => {
-//     const mailOptions = {
-//         from: 'dijarsmakolli99@gmail.com',
-//         to: email,
-//         bcc: 'dijarsmakolli50@gmail.com',
-//         subject: 'Verify your email address',
-//         html: `<p>Please click <a href="http://localhost:6099/verify-email?token=${token}">here</a> to verify your email address.</p>`
-//     };
-
-//     try {
-//         await transporter.sendMail(mailOptions);
-//         console.log('Verification email sent successfully.');
-//     } catch (error) {
-//         console.error("An Error has occurred and we're working to fix the problem!");
-//         console.error(error);
-//         res.status(500).json({
-//             status: 'error',
-//             statusCode: 500,
-//             message: "An Error has occurred and we're working to fix the problem!"
-//         });
-//     }
-// };
