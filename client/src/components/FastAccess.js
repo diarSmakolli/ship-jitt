e0a8ed9632d7e6d22b7d73424c2930a6d15964b8
Dijar Smakolli <dijarprogramming@gmail.com>
2024-05-28 19:41:19 +0200

    feat: updated FastAccess Component.

--- client/src/components/FastAccess.js
+++ client/src/components/FastAccess.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Center, Text, SimpleGrid, Image } from '@chakra-ui/react';
 import { CheckIcon } from '@chakra-ui/icons';
-
+ 
 const FastAccess = () => {
     return (
         <Box>
@@ -14,62 +14,66 @@ const FastAccess = () => {
             >
                 FAST ACCESS
             </Text>
-
-            <SimpleGrid columns={{ base: 1, md: 4 }} spacing={4} mt={10}>
+ 
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4} mt={10}>
                 <Box bg="rgba(0,0,0,.5)" p={9} borderRadius="1.5rem" border='1.5px solid rgba(255,255,255,.12)'>
                     <Image src="https://cdn-icons-png.flaticon.com/128/10490/10490223.png" width='50px' height='50px' />
-
-                    <Text mt={2} color='white' fontSize={'2xl'} fontFamily="Syne" fontWeight={600}>Seamless</Text>
-
+ 
+                    <Text mt={2} color='white' fontSize={'2xl'} fontFamily="Syne" fontWeight={600}>Seamless payments</Text>
+ 
                     <Text mt={2} color='#8b949e' fontSize={'md'} fontFamily={'Poppins'} fontWeight={500}>
-                        <CheckIcon /> Unlock capital in a few easy steps
+                         Effortesly manage transactions with our seamless Stripe integration,
+                        ensuring secure and fast payment processing for your customers.
                     </Text>
-
-
+ 
+ 
                 </Box>
-
+ 
                 <Box bg="rgba(0,0,0,.5)" p={9} borderRadius="1.5rem" border='1.5px solid rgba(255,255,255,.12)'>
                     <Image src="https://cdn-icons-png.flaticon.com/128/10490/10490223.png" width='50px' height='50px' />
-
-                    <Text mt={2} color='white' fontSize={'2xl'} fontFamily="Syne" fontWeight={600}>Seamless</Text>
-
+ 
+                    <Text mt={2} color='white' fontSize={'2xl'} fontFamily="Syne" fontWeight={600}>Custom authentication</Text>
+ 
                     <Text mt={2} color='#8b949e' fontSize={'md'} fontFamily={'Poppins'} fontWeight={500}>
-                        <CheckIcon /> Unlock capital in a few easy steps
+                         Enhance your app security with our custom authentication solutions,
+                        designed to provide robust and reliable user verification.
                     </Text>
-
-
+ 
+ 
                 </Box>
-
+ 
                 <Box bg="rgba(0,0,0,.5)" p={9} borderRadius="1.5rem" border='1.5px solid rgba(255,255,255,.12)'>
                     <Image src="https://cdn-icons-png.flaticon.com/128/10490/10490223.png" width='50px' height='50px' />
-
-                    <Text mt={2} color='white' fontSize={'2xl'} fontFamily="Syne" fontWeight={600}>Seamless</Text>
-
+ 
+                    <Text mt={2} color='white' fontSize={'2xl'} fontFamily="Syne" fontWeight={600}>Automated emails</Text>
+ 
                     <Text mt={2} color='#8b949e' fontSize={'md'} fontFamily={'Poppins'} fontWeight={500}>
-                        <CheckIcon /> Unlock capital in a few easy steps
+                        Streamline your communication with our Mailgun integration,
+                        offering powerful email automation and management capabilites.
                     </Text>
-
-
+ 
+ 
                 </Box>
-
+ 
                 <Box bg="rgba(0,0,0,.5)" p={9} borderRadius="1.5rem" border='1.5px solid rgba(255,255,255,.12)'>
                     <Image src="https://cdn-icons-png.flaticon.com/128/10490/10490223.png" width='50px' height='50px' />
-
-                    <Text mt={2} color='white' fontSize={'2xl'} fontFamily="Syne" fontWeight={600}>Seamless</Text>
-
+ 
+                    <Text mt={2} color='white' fontSize={'2xl'} fontFamily="Syne" fontWeight={600}>Chakra UI components</Text>
+ 
                     <Text mt={2} color='#8b949e' fontSize={'md'} fontFamily={'Poppins'} fontWeight={500}>
-                        <CheckIcon /> Unlock capital in a few easy steps
+                         Develop and customize your application with 20+ ready-to-use components
+                        from Chakra UI, ensuring a consistent and modern design.
                     </Text>
-
-
+ 
+ 
                 </Box>
-
-
-
+ 
+ 
+ 
             </SimpleGrid>
-
+ 
         </Box>
     );
 };
-
+ 
 export default FastAccess;
