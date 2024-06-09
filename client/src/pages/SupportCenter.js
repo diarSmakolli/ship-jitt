121a306b05a0de7c10e9f64ef0556573b5fbbb6b
Dijar Smakolli <dijarprogramming@gmail.com>
2024-06-09 23:37:33 +0200

    feat: finish support center and fix the sign up function.

--- client/src/pages/SupportCenter.js
+++ client/src/pages/SupportCenter.js
@@ -307,98 +307,106 @@ function SupportCenter() {
 
                     <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'> 
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
+                                What do i get in this product?
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                                The Node.js & React.js starter with all boilerplate code you need to run
+                                an online app: a payment system, a database, a user authentication system,
+                                a blog, UI components, emails and more. <br />
+                                
+                                You can use it to build your own SaaS, CRM, CMS, or any other web app.
                             </Text>
 
                         </Box>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'>
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
+                                Can i use PayPal?
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                                Yes, you can send over USD equivalent of the plan you want to purchase
+                                ($169 or $199) to my Paypal account: <Link as='a' color='blue.400' target='_blank' href='https://paypal.me/dijarsmakolli'>paypal.me/dijarsmakolli</Link>
+                                <br />
+                                Once done, please email me your GitHub username so I can give you access to the repo.
+                                <br />
+                                Please allow a few hours to get access.
+
                             </Text>
 
                         </Box>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'>
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
+                                My tech stack is different, can i still use this product?                                                   
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                                Yes, you can. The project is built with Node.js and React.js.
+                                <br />
+                                Libraries is indepedent. You can use SendGrid instead of Mailgun,
+                                MongoDB instead of PostgreSQL, 2Checkout or LemonsQueezy instead of Stripe and so on.
                             </Text>
 
                         </Box>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'>
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
+                                How is Ship Jitt better than other boilerplates?
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                                Ship Jitt is a full-stack starter that includes everything you need to build
+                                a web app. It's not just a UI kit or a set of components. It's a complete
+                                solution that includes a payment system, a database, a user authentication
+                                system, a blog, UI components, emails and more.
                             </Text>
 
                         </Box>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'>
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
+                                Are there any other costs besides the price of the product?
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                                No, there are no other costs. You pay once and get access to the product
+                                and the updates for life. You can use the product for an unlimited number
+                                of projects and clients.
                             </Text>
 
                         </Box>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'>
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
+                                How often do you release updates?
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                                We release updates regularly. You will get access to all the updates for life. <br />
+                                We also provide support for the product. If you have any questions or need help,
+                                you can contact us at any time.
                             </Text>
 
                         </Box>
@@ -430,108 +438,55 @@ function SupportCenter() {
 
                     <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} mt={20}>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'>
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Offerwall issues
+                                English Message
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                                Your message must be in understandable English. Any other language will be ignored. Use google translator if necessary.
                             </Text>
 
                         </Box>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'>
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
+                                Complete description
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                                Your issue must be well described, not just words like "fix it". Take screenshots or record your screen if necessary.
                             </Text>
 
                         </Box>
 
-                        <Box>
+                        <Box bg='gray.900' p={5} rounded='xl'>
 
                             <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
+                                Request for refund
                             </Text>
 
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
+                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'md'} py={5}
                                 fontWeight={400}
                             >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
+                             After you've got access to the repo, ShipJitt is yours forever, so it can't be refunded.
                             </Text>
 
                         </Box>
 
-                        <Box>
-
-                            <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
-                            </Text>
-
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
-                                fontWeight={400}
-                            >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
-                            </Text>
-
-                        </Box>
-
-                        <Box>
-
-                            <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
-                            </Text>
-
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
-                                fontWeight={400}
-                            >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
-                            </Text>
-
-                        </Box>
-
-                        <Box>
-
-                            <Text color='gray.50' fontFamily={'Bricolage Grotesque'} fontSize={'xl'}>
-                                Why my withdrawal is Processing?
-                            </Text>
-
-                            <Text color='gray.300' fontFamily={'Bricolage Grotesque'} fontSize={'lg'} py={5}
-                                fontWeight={400}
-                            >
-                                Some cryptocurrencies are sent using a low fee option. For this reason
-                                your withdrawal get the processing status for a while, it may take up to
-                                24 hours to process.
-                            </Text>
-
-                        </Box>
 
 
                     </SimpleGrid>
                 </Box>
 
 
-                <Box py={24}>
+                <Box mt={24} mb={10}>
                     <Text fontSize='4xl' fontWeight='500' textAlign='center' 
                         fontFamily={'Bricolage Grotesque'} color='gray.50'
                     >
