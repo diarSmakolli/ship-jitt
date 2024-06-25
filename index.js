ef1c0da253ba2b58ecdc9dc501753fc8cc51d219
Dijar Smakolli <dijarprogramming@gmail.com>
2024-06-25 23:48:25 +0200

    ....

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/api/stripe', stripeRoute);
 // const uploadsPath = path.join(__dirname, 'uploads');
 // app.use('/uploads', express.static(uploadsPath));
 
-
+// hint
 
 const sequelize = new Sequelize({
     dialect: process.env.DIALECT,
